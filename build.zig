7ce4d15e0232273184d79bc08dfb057def430ddd
Brian Blumberg <blum@disroot.org>
2021-09-27 20:51:36 -0700

    Change build file

--- build.zig
+++ build.zig
@@ -1,18 +1,20 @@
 const std = @import("std");
 
 pub fn build(b: *std.build.Builder) void {
-    // Standard release options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const mode = b.standardReleaseOptions();
 
     const lib = b.addStaticLibrary("engine", "src/engine.zig");
     lib.setBuildMode(mode);
-    lib.addPackagePath("zlm", "vendor/zlm/zlm.zig");
     lib.install();
 
-    var main_tests = b.addTest("src/main.zig");
-    main_tests.setBuildMode(mode);
+    var tests = b.addTest("src/engine.zig");
+    tests.setBuildMode(mode);
+    tests.addPackagePath("zlm", "vendor/zlm/zlm.zig");
+    tests.linkLibC();
+    tests.linkSystemLibrary("csfml-graphics");
+    tests.linkSystemLibrary("csfml-window");
+    tests.linkSystemLibrary("csfml-system");
 
     const test_step = b.step("test", "Run library tests");
-    test_step.dependOn(&main_tests.step);
+    test_step.dependOn(&tests.step);
 }
