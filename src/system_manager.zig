33fdf6d0a77354830372c52ca5a94d5076d07167
Brian Blumberg <blum@disroot.org>
2021-09-27 17:27:37 -0700

    Allow for init and deinit of system manager

--- src/system_manager.zig
+++ src/system_manager.zig
@@ -1,21 +1,40 @@
 const std = @import("std");
 const log = std.log.scoped(.systemManager);
 
-pub fn SystemManager(comptime S: type, comptime T: type) type {
+pub fn SystemManager(comptime S: type) type {
     return struct {
         const Self = @This();
 
-        systems: []const S,
+        systems: *const Item,
+        inited: bool = false,
+        callDeinit: bool = true,
 
-        pub fn init(systems: []const S) Self {
+        pub const Item = struct {
+            initFn: fn (*S) bool,
+            deinitFn: fn (*S) void,
+            tickFns: []const fn (*S) void,
+        };
+
+        pub fn init(systems: *const Item) Self {
             return .{
                 .systems = systems,
             };
         }
 
-        pub fn tick(self: Self, state: *T) void {
-            for (self.systems) |system| {
-                system.tickFn(state);
+        pub fn run(self: *Self, state: *S) void {
+            if (!self.inited) {
+                self.callDeinit = self.systems.initFn(state);
+                self.inited = true;
+            }
+
+            for (self.systems.tickFns) |system| {
+                system(state);
+            }
+
+            if (self.callDeinit) {
+                self.systems.deinitFn(state);
+                self.callDeinit = false;
+                self.inited = false;
             }
         }
     };
