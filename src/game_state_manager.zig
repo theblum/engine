cafb5f8e19c99c5bf908b69820eda441724ef983
Brian Blumberg <blum@disroot.org>
2021-10-05 19:42:25 -0700

    Add game state transitions

--- src/game_state_manager.zig
+++ src/game_state_manager.zig
@@ -9,8 +9,12 @@ pub fn GameStateManager(comptime S: type, comptime T: type) type {
         current: ?S = null,
         next: ?S,
 
+        transitionFrom: ?TransitionFn = null,
+        transitionTo: ?TransitionFn = null,
+
         const SystemManager = @import("system_manager.zig").SystemManager(T);
         const Array = std.EnumArray(S, Value);
+        const TransitionFn = fn (*T) bool;
         const Value = struct {
             systemManager: SystemManager,
         };
@@ -41,21 +45,47 @@ pub fn GameStateManager(comptime S: type, comptime T: type) type {
             self.array.set(gameState, .{ .systemManager = SystemManager.init(systems) });
         }
 
+        // @Todo: This could probably use some cleanup...
         pub fn run(self: *Self, state: *T) void {
+            var alreadyRun = false;
+
+            if (self.transitionFrom) |transitionFrom| {
+                self.array.getPtr(self.current.?).systemManager.run(state);
+                alreadyRun = true;
+
+                if (transitionFrom(state)) {
+                    return;
+                } else self.transitionFrom = null;
+            }
+
             if (self.next) |next| {
-                if (self.current) |current|
+                if (self.current) |current| {
                     self.array.getPtr(current).systemManager.end(state);
+                }
 
                 self.current = next;
                 self.next = null;
                 self.array.getPtr(self.current.?).systemManager.start(state);
             }
 
-            self.array.getPtr(self.current.?).systemManager.run(state);
+            if (self.transitionTo) |transitionTo| {
+                if (!alreadyRun) {
+                    self.array.getPtr(self.current.?).systemManager.run(state);
+                    alreadyRun = true;
+                }
+
+                if (transitionTo(state)) {
+                    return;
+                } else self.transitionTo = null;
+            }
+
+            if (!alreadyRun) self.array.getPtr(self.current.?).systemManager.run(state);
         }
 
-        pub fn setTo(self: *Self, next: S) void {
+        pub fn setTo(self: *Self, next: S, transitionFrom: ?TransitionFn, transitionTo: ?TransitionFn) void {
             self.next = next;
+            self.transitionFrom = transitionFrom;
+            self.transitionTo = transitionTo;
         }
     };
 }
